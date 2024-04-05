65b9e604320ac1243d1dba4a3a2fed4b0166a3b0
Vamshix57 <vamshi.krishna@kalvium.community>
2024-04-05 10:41:39 +0530

    Done

---
+++ Frontend/client/src/components/LandingPage.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "../App.css"
+const LandingPage = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/Cars")
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className="maincontainer"> 
+        {data.map((car, i) => (
+          <div className="card" key={i}>
+            <img src={car.img} alt={car.name} className="card-img-top" />
+            <div className="card-body">
+              <h5 className="card-title">{car.name}</h5>
+              <p className="card-text">Rating: {car.rating}</p>
+              <p className="card-text">Year of Launch: {car.year_of_launch}</p>
+              <p className="card-text">Price: ${car.price}</p>
+              <p className="card-text">Company: {car.company}</p>
+              <p className="card-text">Mileage: {car.mileage} kmpl</p>
+              <button className="delete">Delete</button>
+              <button className="update">Update</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default LandingPage;
