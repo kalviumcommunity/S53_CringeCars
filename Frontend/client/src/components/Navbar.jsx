e114c3eca33155c24b152d59402e2fbfa5281a86
Vamshix57 <vamshi.krishna@kalvium.community>
2024-04-16 15:01:43 +0530

    cookie data

--- Frontend/client/src/components/Navbar.jsx
+++ Frontend/client/src/components/Navbar.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 function NavBar() {
+  // State to track if the user is logged in
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // Function to handle logout
+  const handleLogout = () => {
+    // Set the expiration date of the access token cookie to a past time
+    document.cookie = "access_token=;expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+    // Update state to reflect logout
+    setIsLoggedIn(false);
+  };
+
+  useEffect(() => {
+    // Check if access token cookie exists when component mounts
+    const accessTokenCookie = document.cookie
+      .split(";")
+      .find((cookie) => cookie.trim().startsWith("access_token="));
+    setIsLoggedIn(!!accessTokenCookie);
+  }, []);
+
   return (
     <nav className="navbar-dark">
       <div className="navbar-container">
@@ -11,15 +30,30 @@ function NavBar() {
         <Link to="/aboutus" className="navbar-link">
           About us
         </Link>
-        <Link to='/' className="navbar-link">
+        <Link to="/" className="navbar-link">
           Contact
         </Link>
-        <Link to="/signup" className="navbar-link">
-          Sign up
-        </Link>
-        <Link to="/carform" className="navbar-link">
-          Post
-        </Link>
+        {/* Conditional rendering based on user login status */}
+        {!isLoggedIn && (
+          <>
+            <Link to="/signup" className="navbar-link">
+              Sign up
+            </Link>
+            <Link to="/login" className="navbar-link">
+              Login
+            </Link>
+          </>
+        )}
+        {isLoggedIn && (
+          <>
+            <Link to="/carform" className="navbar-link">
+              Post
+            </Link>
+            <button className="navbar-button" onClick={handleLogout}>
+              Logout
+            </button>
+          </>
+        )}
       </div>
     </nav>
   );
